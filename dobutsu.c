15a4421a7951856a555cb664aeeeed01651ee208
Robert Clausecker <fuz@fuz.su>
2017-01-03 02:29:58 +0100

    dobutsu: More progress

--- dobutsu.c
+++ dobutsu.c
@@ -1,6 +1,8 @@
 #define _POSIX_C_SOURCE 200809L
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 
@@ -13,19 +15,70 @@
 struct gamestate {
 	struct gamestate *previous;
 	struct position position;
-	struct move current_move;
+	struct move next_move;
 	unsigned move_clock;
+};
+
+/*
+ * The engine may play any combination of Sente and Gote, including
+ * no player.
+ */
+enum {
+	ENGINE_NONE = 0,
+	ENGINE_SENTE = 1,
+	ENGINE_GOTE = 2,
+	ENGINE_SENTE_GOTE = 3
 };
 
 /* global variables */
 static struct tablebase *tb = NULL;
 static struct gamestate *gs = NULL;
 
+static unsigned char engine_players = 0;
+
 /* internal functions */
 static void	open_tablebase(void);
-static void	end_game(void);
-static void	new_game(void);
 static void	execute_command(char *);
+static void	end_game(void);
+static void	new_game(const char *);
+static void	cmd_exit(const char *);
+static void	cmd_show(const char *);
+static void	cmd_undo(const char *);
+static void	cmd_remove(const char *);
+static void	play(struct move m);
+static void	autoplay(void);
+static int	undo(void);
+
+/*
+ * This table contains all available commands.  New commands should be
+ * added to this table.  The callback function takes as its sole
+ * argument the second word of the command.  The member words indicates
+ * how many words a command of this kind takes.  If a wrong number of
+ * words is passed, an error is produced instead.  The table is
+ * terminated with an entry containing NULL for the function pointer.
+ */
+static const struct {
+	void (*callback)(const char *);
+	unsigned char words;
+	char command[12];
+} commands[] = {
+	cmd_exit,	1, "exit",
+	cmd_exit,	1, "quit",
+	new_game,	1, "new",
+	cmd_show,	2, "show",
+	cmd_undo,	1, "undo",
+	cmd_remove,	1, "remove",
+
+	/* unimplemented commands */
+/*	cmd_help,	1, "help",
+	cmd_hint,	1, "hint",
+	cmd_version,	1, "version",
+	cmd_go,		1, "go",
+	cmd_force,	1, "force",
+	cmd_setboard,	2, "setboard",
+*/
+	NULL,		0, "",
+};
 
 extern int
 main(int argc, char *argv[])
@@ -42,21 +95,12 @@ main(int argc, char *argv[])
 	setbuf(stdout, NULL);
 
 	open_tablebase();
-	new_game();
+	new_game(NULL);
 
 	while (getline(&linebuf, &linebuflen, stdin) > 0)
 		execute_command(linebuf);
 
-	end_game();
-	free_tablebase(tb);
-	tb = NULL;
-
-	if (ferror(stdin)) {
-		perror("Error reading command");
-		return (EXIT_FAILURE);
-	}
-
-	return (EXIT_SUCCESS);
+	cmd_exit(NULL);
 }
 
 /*
@@ -108,11 +152,13 @@ end_game()
 
 /*
  * Start a new game by clearing the old game state and initializing it
- * with the state of a new game.
+ * with the state of a new game.  The first argument must be NULL.
  */
 static void
-new_game()
+new_game(const char *arg)
 {
+
+	assert(arg == NULL);
 
 	end_game();
 	gs = malloc(sizeof *gs);
@@ -132,5 +178,170 @@ new_game()
 static void
 execute_command(char *cmd)
 {
-	printf("Error (unknown command): %s", cmd);
+	struct move m;
+	size_t i;
+	unsigned words;
+	char *context, *word1, *word2, *word3;
+
+	/* trim newline if any */
+	cmd[strcspn(cmd, "\r\n")] = '\0';
+
+	if (parse_move(&m, &gs->position, cmd) == 0) {
+		play(m);
+		autoplay();
+		return;
+	}
+
+	word1 = strtok_r(cmd, " ", &context);
+	word2 = strtok_r(NULL, " ", &context);
+	word3 = strtok_r(NULL, " ", &context);
+
+	if (word1 == NULL)
+		return;
+	else if (word2 == NULL)
+		words = 1;
+	else if (word3 == NULL)
+		words = 2;
+	else
+		words = 3;
+
+	for (i = 0; commands[i].callback != NULL; i++) {
+		if (strcmp(commands[i].command, word1) != 0 || words != commands[i].words)
+			continue;
+
+		commands[i].callback(word2);
+		return;
+	}
+
+	printf("Error (unknown command): %s\n", cmd);
+}
+
+/*
+ * Play m on the current game state and update gs appropriately.
+ */
+static void
+play(struct move m)
+{
+	struct gamestate *newgs = malloc(sizeof *newgs);
+
+	if (newgs == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	newgs->previous = gs;
+	newgs->position = gs->position;
+	newgs->move_clock = gs->move_clock + 1;
+	gs->next_move = m;
+	gs = newgs;
+
+	if (play_move(&gs->position, m)) {
+		printf("You won!\nStarting new game.\n");
+		new_game(NULL);
+	}
+}
+
+/*
+ * Close everything and exit the program.  This function should not
+ * return.  The first argument is ignored.
+ */
+static void
+cmd_exit(const char *arg)
+{
+
+	assert(arg == NULL);
+
+	end_game();
+	free_tablebase(tb);
+	tb = NULL;
+
+	if (ferror(stdin)) {
+		perror("Error reading command");
+		exit(EXIT_FAILURE);
+	}
+
+	exit(EXIT_SUCCESS);
+}
+
+/*
+ * The show command has several subcommands.  Due to the small number of
+ * subcommands, we call them directly through an if-cascade.
+ */
+static void
+cmd_show(const char *arg)
+{
+	struct move moves[MAX_MOVES];
+	size_t i, n;
+	char render[MAX_RENDER], movstr[MAX_MOVSTR];
+
+	assert(arg != NULL);
+
+	if (strcmp(arg, "board") == 0) {
+		position_render(render, &gs->position);
+		fputs(render, stdout);
+	} else if (strcmp(arg, "moves") == 0) {
+		n = generate_moves(moves, &gs->position);
+		for (i = 0; i < n; i++) {
+			move_string(movstr, &gs->position, moves[i]);
+			puts(movstr);
+		}
+	} else
+		printf("Error (unknown command): show %s\n", arg);
+}
+
+/*
+ * The undo command lets the player undo a single move.
+ */
+static void
+cmd_undo(const char *arg)
+{
+
+	assert(arg == NULL);
+	if (undo())
+		autoplay();
+}
+
+/*
+ * The remove command lets the player undo two moves.  This is
+ * useful when you are playing against the engine.
+ */
+static void
+cmd_remove(const char *arg)
+{
+
+	assert(arg == NULL);
+	if (undo() && undo())
+		autoplay();
+}
+
+/*
+ * undo undoes the last move played if such a move existed.  This
+ * function returns nonzero if a move to be undone exists.
+ */
+static int
+undo(void)
+{
+	struct gamestate *oldgs = gs;
+
+	if (oldgs->previous != NULL) {
+		gs = oldgs->previous;
+		free(oldgs);
+		return (1);
+	} else {
+		printf("Nothing to undo.\n");
+		return (0);
+	}
+}
+
+/*
+ * If it's the engine's turn, play a move for the engine.  Repeat until
+ * either it's no longer the engine's turn or (if both players are
+ * engine players) until the game ends.
+ */
+static void
+autoplay(void)
+{
+
+	/* TODO */
+	return;
 }
